b5e94c5c77a957aef9771f6f4355f9714c5657af
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2026-03-24 01:00:13 -0400

    project start. scaffolded basic site structure and style and added photos for testing

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+const mobileMenuLinks = document.getElementsByClassName("mobile-link");
+const photoSection = document.getElementById("photo-section");
+
+function closeMobileMenuOnResize() {
+  mobileMenuClassList = Array.from(mobileMenu.classList);
+  if (window.innerWidth > 500 && mobileMenuClassList.includes("open")) {
+    toggleMobileMenu();
+  }
+}
+window.addEventListener("resize", closeMobileMenuOnResize);
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("open");
+}
+
+function createPhotoSection() {
+  for (let i = 1; i < 16; i++) {
+    let photo = document.createElement("img");
+    photo.classList += "photos"
+    photo.src += `../photos/Test_Image_${i}.jpg`
+    photoSection.appendChild(photo);
+  }
+}
+
+createPhotoSection();
